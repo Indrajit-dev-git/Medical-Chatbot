817bbb6d21e87fc6b78aec2dc2c2a8c6d00a4fd4
indraeng <indrajitnaskar247@gmail.com>
2024-11-19 23:35:46 +0530

    completed

--- template.py
+++ template.py
@@ -7,6 +7,7 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 list_of_files = [
     "src/_init_.py",
     "src/helper.py",
+    "src/prompt.py",
     ".env",
     "setup.py",
     "app.py",
